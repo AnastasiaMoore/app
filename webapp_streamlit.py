1dccac9d9a3f077ffd8beeb474539a8d34513c94
stassy8 <71414664+stassy8@users.noreply.github.com>
2020-12-11 03:37:38 -0700

    Update webapp_streamlit.py

--- webapp_streamlit.py
+++ webapp_streamlit.py
@@ -7,22 +7,17 @@ st.write("""
          )
 st.write("This is a simple image classification web app to classify the road signs")
 file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
-
 import cv2
 from PIL import Image, ImageOps
 import numpy as np
 def import_and_predict(image_data, model):
-    
         size = (100,100)    
         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
         image = np.asarray(image)
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         img_resize = (cv2.resize(img, dsize=(100, 100),    interpolation=cv2.INTER_CUBIC))/255.
-        
         img_reshape = img_resize[np.newaxis,...]
-    
         prediction = model.predict(img_reshape)
-        
         return prediction
 if file is None:
     st.text("Please upload an image file")
@@ -30,7 +25,6 @@ else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
     prediction = import_and_predict(image, model)
-    
     score = tf.nn.softmax(prediction) 
     st.write("The sign corresponds to", np.argmax(prediction), "ClassId.")
     st.write("Confidence:",  round(100 * np.max(score), 1), "%")
