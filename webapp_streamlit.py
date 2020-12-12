edc5026ebf6e4e0c914553a09d3c4afca2fc09be
stassy8 <71414664+stassy8@users.noreply.github.com>
2020-12-11 21:14:59 -0700

    Update webapp_streamlit.py

--- webapp_streamlit.py
+++ webapp_streamlit.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
+from tensorflow import keras
 model = tf.keras.models.load_model('my_model.hdf5')
+
 import streamlit as st
 st.write("""
          # German Street Road Sign Prediction
@@ -7,24 +9,21 @@ st.write("""
          )
 st.write("This is a simple image classification web app to classify the road signs")
 file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
+
 import cv2
 from PIL import Image, ImageOps
 import numpy as np
 def import_and_predict(image_data, model):
-        size = (100,100)    
-        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-        image = np.asarray(image)
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        img_resize = (cv2.resize(img, dsize=(100, 100),    interpolation=cv2.INTER_CUBIC))/255.
-        img_reshape = img_resize[np.newaxis,...]
-        prediction = model.predict(img_reshape)
-        return prediction
+    img_array = keras.preprocessing.image.img_to_array(image_data)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+    prediction = model.predict(img_array)
+    return prediction
 if file is None:
-    st.text("Please upload an image file")
+    st.text("You haven't uploaded an image file")
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
     prediction = import_and_predict(image, model)
     score = tf.nn.softmax(prediction) 
-    st.write("The sign corresponds to", np.argmax(prediction), "ClassId.")
+    st.write("The sign corresponds to", np.argmax(score), "ClassId.")
     st.write("Confidence:",  round(100 * np.max(score), 1), "%")
